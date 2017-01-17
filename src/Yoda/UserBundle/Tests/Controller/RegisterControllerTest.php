3625dd4f79a6a06762c2bffd18afbf1f7a1d2eaa
dontsova <v.koroliev@gmail.com>
2017-01-17 13:49:06 +0200

    ep2 #31.2 done

---
+++ src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+// src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
+namespace Yoda\UserBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegisterControllerTest extends WebTestCase
+{
+    public function testRegister()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register');
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Register', $response->getContent());
+
+        $usernameVal = $crawler
+            ->filter('#user_register_username')
+            ->attr('value')
+        ;
+        $this->assertEquals('Foo', $usernameVal);
+    }
+}
